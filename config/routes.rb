76dc6593128929431c52197a2892bb2e24b2a59d
Igor Serpak <igor.serpak@gmail.com>
2015-03-04 12:45:58 +0200

    'Create a new project' feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ticketee::Application.routes.draw do
+
+  root "projects#index"
+
+  resources :projects
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +43,7 @@ Ticketee::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
